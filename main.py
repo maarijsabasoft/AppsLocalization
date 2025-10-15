03ba61b8461f79db1155f5f9905f92b5f450e4d3
maarijsabasoft <maarij.sabasoft@gmail.com>
2025-10-15 09:22:09 +0500

    Update main.py1111

--- main.py
+++ main.py
@@ -718,5 +718,14 @@ def download_file():
     flash("No image available for download.", "error")
     return redirect(url_for("index"))
 
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
